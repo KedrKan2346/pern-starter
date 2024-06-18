baed918041d82d21f118654fc98b4843baf08152
kedr-kan <157804914+KedrKan2346@users.noreply.github.com>
2024-06-17 21:01:14 -0400

    Added xss validation

--- api/src/features/subject/presentation/validators.ts
+++ api/src/features/subject/presentation/validators.ts
@@ -1,10 +1,16 @@
 import { z } from 'zod';
+import { filterXSS } from 'xss';
 
 /**
  * Subject's PUT and POST HTTP request body validation schema.
  */
 export const subjectCreateOrUpdateDtoSchema = z.object({
-  name: z.string().max(255),
+  name: z
+    .string()
+    .max(255)
+    .refine((data) => {
+      return filterXSS(data) === data;
+    }),
   sex: z.string().max(6),
   status: z.string().max(50),
   diagnosisDate: z.string().max(50),
